f7a913d94987f9ddde8a582e40e8b57888b56636
ychxexn <ychxexn@gmail.com>
2022-08-30 23:43:38 +0900

    상품 조회 API 추가

---
+++ src/main/java/com/example/catalogservice/controller/CatalogController.java
@@ -0,0 +1,50 @@
+package com.example.catalogservice.controller;
+
+import com.example.catalogservice.jpa.CatalogEntity;
+import com.example.catalogservice.service.CatalogService;
+import com.example.catalogservice.vo.ResponseCatalog;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog-service")
+@RequiredArgsConstructor
+public class CatalogController {
+
+    private final Environment env;
+
+    private final CatalogService catalogService;
+
+    @GetMapping("/health_check")
+    public String status(){
+        return String.format("It's working in catalog service on PORT %s", env.getProperty("local.server.port"));
+    }
+
+    @GetMapping("/catalogs")
+    public ResponseEntity<List<ResponseCatalog>> getCatalogs() {
+        Iterable<CatalogEntity> catalogList = catalogService.getAllCatalogs();
+
+        List<ResponseCatalog> result = new ArrayList();
+        catalogList.forEach(catalogEntity -> result.add(new ModelMapper().map(catalogEntity, ResponseCatalog.class)));
+
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @GetMapping("/catalogs/{productId}")
+    public ResponseEntity<ResponseCatalog> getCatalog(@PathVariable String productId) {
+        CatalogEntity catalogEntity = catalogService.getCatalogByProductId(productId);
+        ResponseCatalog result = new ModelMapper().map(catalogEntity, ResponseCatalog.class);
+
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+}
